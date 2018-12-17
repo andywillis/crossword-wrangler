b6c4df12fc51a9c1f6f0449656f9d693961dcd57
Andy Willis <github@awillis.fastmail.fm>
2018-12-17 23:30:56 +0000

    Added JSON output

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ function getWeekDayCalendar(year) {
 uris.easy = getWeekDayCalendar(2018).map(date => `${uri}easy_${date}.xml`);
 uris.quick = getWeekDayCalendar(2018).map(date => `${uri}quic_${date}.xml`);
 
-console.log(JSON.stringify(uris));
+// console.log(JSON.stringify(uris));
 
 function verifyFolderExists(path) {
   return new Promise((resolve, reject) => {
@@ -57,23 +57,44 @@ function verifyFolderExists(path) {
 
 const root = path.join(__dirname);
 
-function setupFolders() {
+function addFolder(folderName) {
   return new Promise(async (resolve) => {
-    await verifyFolderExists(`${root}/data/easy`);
-    await verifyFolderExists(`${root}/data/quick`);  
+    await verifyFolderExists(`${root}/data/${folderName}`);
     resolve();
   });
 }
 
-setupFolders().then(() => {
-  uris.easy.forEach(link => throttle(async () => {
-    const filePath = path.join(__dirname, 'data/easy');
-    const filename = link.split('/').pop();
-    request(link).pipe(fs.createWriteStream(`${filePath}/${filename}`));
+
+async function getCrosswords(type) {
+  await addFolder(type);
+  uris[type].slice(0, 1).forEach(link => throttle(async () => {
+    const filePath = path.join(__dirname, `data/${type}`);
+    const filename = link.split('/').pop().replace('xml', 'json');
+    request(link, (err, res, body) => {
+      if (err) console.log(err);
+      parseString(body, (err, res) => {
+        if (err) console.log(err);
+
+        const { crossword } = res['crossword-compiler']['rectangular-puzzle'][0];
+        const { grid, word, clues: cluesArr } = crossword[0];
+        const { $: { width, height }, cell } = grid[0];
+
+        const cells = cell.map(o => o.$);
+        const words = word.map(o => o.$);
+
+        const clues = cluesArr[0].clue.map((o) => {
+          const { _: clue, $: meta } = o;
+          return { clue, meta };
+        });
+
+        const obj = { width, height, cells, words, clues };
+        const json = JSON.stringify(obj, null, 2);
+        fs.writeFile(`${filePath}/${filename}`, json, (err) => {
+          if (err) console.log(err);
+        });
+      });
+    }); // .pipe(fs.createWriteStream(`${filePath}/${filename}`));
   }));
-  uris.quick.forEach(link => throttle(async () => {
-    const filePath = path.join(__dirname, 'data/quick');
-    const filename = link.split('/').pop();
-    request(link).pipe(fs.createWriteStream(`${filePath}/${filename}`));
-  }));
-});
+}
+
+getCrosswords('easy');
