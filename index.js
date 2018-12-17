34ae21d21eb844c56d3507dc7c0de209acdb8518
Andy Willis <github@awillis.fastmail.fm>
2018-12-17 21:08:43 +0000

    Save files

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const request = require('request');
+const fs = require('fs');
+const path = require('path');
+const createThrottle = require('async-throttle');
+const { parseString } = require('xml2js');
 
-const year = 2018;
-const dateArr = [];
+const throttle = createThrottle(2);
+
 const uris = { easy: [], quick: [] };
 const uri = 'https://ams.cdn.arkadiumhosted.com/assets/gamesfeed/evening-standard/daily-crossword/';
 
@@ -36,3 +40,40 @@ uris.easy = getWeekDayCalendar(2018).map(date => `${uri}easy_${date}.xml`);
 uris.quick = getWeekDayCalendar(2018).map(date => `${uri}quic_${date}.xml`);
 
 console.log(JSON.stringify(uris));
+
+function verifyFolderExists(path) {
+  return new Promise((resolve, reject) => {
+    fs.stat(path, (err) => {
+      if (err) {
+        fs.mkdir(path, (err) => {
+          if (err) reject(err);
+          resolve();
+        });
+      }
+      resolve();
+    });
+  });
+}
+
+const root = path.join(__dirname);
+
+function setupFolders() {
+  return new Promise(async (resolve) => {
+    await verifyFolderExists(`${root}/data/easy`);
+    await verifyFolderExists(`${root}/data/quick`);  
+    resolve();
+  });
+}
+
+setupFolders().then(() => {
+  uris.easy.forEach(link => throttle(async () => {
+    const filePath = path.join(__dirname, 'data/easy');
+    const filename = link.split('/').pop();
+    request(link).pipe(fs.createWriteStream(`${filePath}/${filename}`));
+  }));
+  uris.quick.forEach(link => throttle(async () => {
+    const filePath = path.join(__dirname, 'data/quick');
+    const filename = link.split('/').pop();
+    request(link).pipe(fs.createWriteStream(`${filePath}/${filename}`));
+  }));
+});
